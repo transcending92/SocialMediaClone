0b8891148ac4f24ee874e782a045361471074ad6
transcending92 <melaniekealacampbell@gmail.com>
2022-05-13 21:53:02 -0500

    Json is working properly by being added to firebase, Adding additional users

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
-var selectedRow = true
+window.addEventListener('load', () => {
 
-function validateUserCreds(e) {
-	event.preventDefault();
-        var formData = readFormData();
-        if (selectedRow == true){
-            console.log("User is granted access")
-		}
-        else{
-            console.log("Invalid username or password!");
-		}
-        resetForm();    
-}
 
-//Retrieve the data
-function readFormData() {
-    var formData = {};
-    formData[]
+//When clicking the submit button, display an error message for invalid username or password
+        center.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const username = document.querySelector("#username");
+        const password = document.querySelector("#password");
+        const form = document.getElementById("#form");
+        const center = document.getElementById("#center");
+        
+
+        if (username.value === "CodemaniaChica_92" && password.value === "Codeexample1") {
+            alert("Login Successful");
+            
+        } else {
+            alert("Invalid username or password!");
+     }
+    
+})
+
+
+
+
+
+
+
+
 }
+)
+
+
+
+
+
+
+
+
+
+
+
+
--- nav.js
+++ nav.js
@@ -1,24 +1,76 @@
-// Selectors - use to Post, Put, Patch, Delete, Get from firebase 
-const $getBtn = $("#getBtn");
-const $postBtn = $("#postBtn");
-const $deleteBtn = $("#deleteBtn");
-const $patchBtn = $("#patchBtn");
-const $putBtn = $("#putBtn");
+// // Selectors - use to Post, Put, Patch, Delete, Get from firebase 
+// const $getBtn = $("#getBtn");
+// const $postBtn = $("#postBtn");
+// const $deleteBtn = $("#deleteBtn");
+// const $patchBtn = $("#patchBtn");
+// const $putBtn = $("#putBtn");
 
-let firebaseURL = "https://snackgram-63aca-default-rtdb.firebaseio.com/";
-let jsonEXT = ".json";
+// let firebaseURL = "https://snackgram-63aca-default-rtdb.firebaseio.com/";
+// let jsonEXT = ".json";
 
-//GET Method
-$getBtn.click(() => {
-    $.ajax({
-        type: "GET",
-        url: `${firebaseURL}${jsonEXT}`,
-        success: (username == ture || password == true) => {
-            console.log("Access Granted");
-        }
-        error: (error) => {
-            console.log("Invalid Username or password!");
-        }
-    })
-}
-)
+// //GET Method
+// $getBtn.click(() => {
+//     $.ajax({
+//         type: "GET",
+//         url: `${firebaseURL}${jsonEXT}`,
+//         success: (username == ture || password == true)=> {
+//             console.log("Access Granted");
+//         }
+//         error: (error) => {
+//             console.log("Invalid Username or password!")
+//         }
+// })
+// })
+
+
+
+// -----------------------------------------------
+// potential JSON
+//     },{
+//       "Melissa" : {
+//             "username" : "Livingmybestlife",
+//             "password" : "Codeexample2",
+//             "nickname" : "Fefe",
+//             "posts" :{
+//                 "image" :,
+//                 "time" : "18:34:40",
+//                 "content": "",
+//                 "likes" : "120",
+//                 "comments" : [{
+//                     "username" : "SillyRabbit",
+//                     "comment" : "Tricks are for kids"
+//                 }]
+//         }  
+//     },
+//         "Merline" : {
+//              "username" : "TrinityVibesFire",
+//             "password" : "Codeexample3",
+//             "nickname" : "Trini",
+//             "posts" :{
+//                 "image" :,
+//                 "time" : "12:00:00",
+//                 "content": "Come travel with me",
+//                 "likes" : "1920",
+//                 "comments" : [{
+//                     "username" : "_Hi_Lady_",
+//                     "comment" : "Nice photo"
+//                     }]
+//         }  
+//     },
+//         "Candace"
+//                 "username" : "Love>HateAnyday",
+//                 "password" : "Codeexample4",
+//                 "nickname" : "Monique",
+//                 "posts" :{
+//                     "image" :,
+//                     "time" : "12:00:00",
+//                     "content": "Living my best life",
+//                     "likes" : "2022",
+//                     "comments" : [
+//                         "username" : "Precious",
+//                         "comment" : "That's cool",
+//                 ]
+//             }
+//         }
+//     }
+// }
